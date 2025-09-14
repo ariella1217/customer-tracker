ee2b93b52c2f2a69b76dcea25a45fdf3d5faa821
Ariella <ariellayavno@usf.edu>
2025-09-14 15:10:19 -0400

    fixed code spacing again

--- cc_3.js
+++ cc_3.js
@@ -2,21 +2,21 @@ let customers = [
 
 { name:"Robert",
   email:"Robert2001@gmail.com",
-  purchases:["Snickers ","KitKat ","Milky Way "]
+  purchases:["Snickers"," KitKat"," Milky Way"]
 },
 
 { name:"Elizabeth",
   email:"Elizabeth1024@gmail.com",
-  purchases:["Takis ","Salt & Vinegar Lays ","Popcorn"]
+  purchases:["Takis"," Salt & Vinegar Lays"," Popcorn"]
 },
 
 { name:"Isabella",
   email:"Isabella13@yahoo.com",
-  purchases:["eyeshadow ","mascara ","liptstick "]
+  purchases:[" eyeshadow"," mascara"," liptstick"]
 }
 ];
 
-newCustomer = {name:"Jacob",email:"Jacob2001@gmail.com",purchases:["soccer ball ","baseball ","basketball "]}
+newCustomer = {name:"Jacob",email:"Jacob2001@gmail.com",purchases:[" soccer ball"," baseball"," basketball"]}
 customers.push(newCustomer)
 console.log(customers);
 
